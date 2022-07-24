38e5437a4ceaf8df01f2f6bec1fcb6b0fefe3ba1
Vad <example@gmail.com>
2022-07-24 09:31:23 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './components/app/app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+// reportWebVitals();
+
+
+"use strict";
+
+
+const result = confirm('Are you here?');
+console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer);
+
+// let incr = 10,
+//     decr = 10;
+
+
+//     incr++;
+//     decr--;
+
+//     console.log(incr);
+//     console.log(decr);
