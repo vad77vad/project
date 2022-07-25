7021a9dad6a9fe7b5bf802fd5d2b78612c58591c
Vad <example@gmail.com>
2022-07-25 17:07:56 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -15,9 +15,8 @@
 
 "use strict";
 
-
-const result = confirm('Are you here?');
-console.log(result);
+// const result = confirm('Are you here?');
+// console.log(result);
 
 // const answer = prompt('Вам есть 18?', '18');
 // console.log(answer);
@@ -31,3 +30,27 @@ console.log(result);
 
 //     console.log(incr);
 //     console.log(decr);
+
+
+const numberOfFilms = +prompt ('Сколькo фильмов вы посмотрели?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('oдин из просмотренних фильмов'),
+      b = prompt('На сколько его оцените?'),  
+      c = prompt('oдин из просмотренних фильмов'),
+      d = prompt('На сколько его оцените?');  
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
